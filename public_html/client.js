d3d82475c5a1450704280fbf5f0e6a483d25aa34
strobo <kax551@gmail.com>
2012-03-05 20:18:03 +0900

    tree graph is working by sample data!

--- public_html/client.js
+++ public_html/client.js
@@ -1,28 +1,31 @@
-function NoteCell(html, id){
-    this.html = html;
+function NoteCell(html, id, name){
+    //this.html = html;
+    this.data = { html:html };
     this.id = id;
-    this.child = new Array();
-    this.addChild = function(html, id){
-        this.child.push(new NoteCell(html, id));
+    this.name = name;
+    this.children = new Array();
+    this.addChild = function(html, id, name){
+        this.children.push(new NoteCell(html, id, name));
     }
 }
-function NoteTree(){
-    this.html;
-    this.name;
-    this.url;
-    this.child = new Array();
-
-
-    this.addChild = function(html, name, url){
-        this.child.push(new NoteCell(html, name, url));
-    }
-
-}
-function PreNote(){
-    this.note = new Array();
-    this.tree = new NoteTree();
-    this.add = function(img_link_url, img_url, dst_url, dst_uname, src_url, src_uname){
-        this.note.push({
+//function NoteTree(){
+//    this.html;
+//    this.name;
+//    this.url;
+//    this.child = new Array();
+//
+//
+//    this.addChild = function(html, name, url){
+//        this.child.push(new NoteCell(html, name, url));
+//    }
+//
+//}
+function Note(){
+    this.list= new Array();
+    this.tree = new NoteCell();
+    
+    this.addList = function(img_link_url, img_url, dst_url, dst_uname, src_url, src_uname){
+        this.list.push({
             img_link_url : img_link_url,
             img_url      : img_url,
             dst_url      : dst_url,
@@ -31,39 +34,60 @@ function PreNote(){
             src_uname    : src_uname
         });
     }
-        this.noteToTree = function(){
-            for(var i = this.note.length - 1; i >= 0; i--){
-                var noteCell = this.note[i];
-                var html = '<a href="'+ noteCell.dst_url +'"><img src="'+ noteCell.img_url + '">'+ noteCell.dst_uname +'</a>';
-                var id = this.note.length - 1 - i;
-                if(id === 0){
-                    this.tree = new NoteCell(html, id);
-                    continue;
-                }
-                //this.tree.
+    this.getTreeByName = function(name, obj){
+        for(var i in obj){
+            if(i === "name"){
+                if(obj[i] === name) return obj;
             }
+            if(i === "children"){
+                for(var j in obj[i]){
+                    var res = this.getTreeByName(name, obj[i][j]);
+                    if(res === "notFound") continue;
+                    return res;
+                }
+                return "notFound";
+            }
+        }
+        return "notFound";
+    }
+    this.listToTree = function(){
+        for(var i = this.list.length - 1; i >= 0; i--){
+            var noteCell = this.list[i];
+            var html = '<a href="'+ noteCell.dst_url +'"><img src="'+ noteCell.img_url + '">'+ noteCell.dst_uname +'</a>';
+            var id = this.list.length - 1 - i;
+            var name = noteCell.dst_uname;
+            
+            // when first loop. create initial note
+            if(id === 0){       
+                this.tree = new NoteCell(html, id, name);
+                continue;
+            }
+            var src_note = this.getTreeByName(noteCell.src_uname, this.tree);
+            if(src_note === "notFound") continue;
+            src_note.addChild(html, id, name);
+        }
     }
 }
 
 
 
-prenote = new PreNote();
+var note = new Note();
 var st;
 $(function(){
     console.log("start");
 
-    //noteTree = new NoteTree();
     url = "http://strobot.tumblr.com/post/18123513647/kamatama-udon-cherrypin";
 
     /*$.get("/",{"q":url},function(res){
       console.log(res);
       });*/
     $("#notedata").append(html);
+    //notehtml = Jquery(html);
     $("#notedata li[class*=reblog]").css("background-color", "yellow")
     $("#notedata li[class*=reblog]").each(function(){
         //console.log(this);
         if(this.children[1].childElementCount == 1){    // foo posted this
-            prenote.add(
+            note.addList(
                 this.children[0].href,                  //img_link_url
                 this.children[0].children[0].src,       //img_url
                 this.children[1].children[0].href,      //dst_url
@@ -72,7 +96,7 @@ $(function(){
                 null //src_usrname
                 );
         } else {
-            prenote.add(
+            note.addList(
                 this.children[0].href,                  //img_link_url
                 this.children[0].children[0].src,       //img_url
                 this.children[1].children[0].href,      //dst_url
@@ -82,6 +106,7 @@ $(function(){
                 );
         }
     });
+    note.listToTree();
 
     st = new $jit.ST({
        injectInto: 'graph',
@@ -115,6 +140,7 @@ $(function(){
 
        onBeforeCompute: function(node){
            console.log("loading " + node.name);
+           console.log(node);
        },
 
        onAfterCompute: function(){
@@ -122,7 +148,7 @@ $(function(){
        },
        onCreateLabel: function(label, node){
            label.id = node.id; 
-           label.innerHTML = node.name;
+           label.innerHTML = node.data.html;
            //set label styles
            var style = label.style;
            //style.width = 60 + 'px';
@@ -136,9 +162,9 @@ $(function(){
        }
     });
 
-    //st.loadJSON(json);
-    //st.compute();
-    //st.onClick(st.root);
-    //st.switchPosition('top','replot');
+    st.loadJSON(note.tree);
+    st.compute();
+    st.onClick(st.root);
+    st.switchPosition('top','replot');
 
 });
